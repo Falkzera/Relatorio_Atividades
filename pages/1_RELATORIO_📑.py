373ea3bdcb2fccf19cba2537b0cc1e724c62128f
Falkzera <falcovisk@gmail.com>
2025-02-04 18:28:49 -0300

    Update email

--- pages/1_RELATORIO_📑.py
+++ pages/1_RELATORIO_📑.py
@@ -1,12 +1,13 @@
 import os
+import json
 import io
+import smtplib
 import calendar
-from datetime import datetime
-
 import pandas as pd
+from googleapiclient.http import MediaIoBaseUpload, MediaIoBaseDownload # type: ignore
 import streamlit as st
 import plotly.graph_objects as go
-
+from datetime import datetime
 from Scripts.google_drive_utils import (
     authenticate_service_account,
     create_folder_in_drive,
@@ -16,7 +17,10 @@ from Scripts.google_drive_utils import (
     read_parquet_files_from_drive
 )
 from Models.marca import display_sidebar, display_header
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 st.set_page_config(layout='wide', page_title='Relatório de Atividades', page_icon='📊')
 
@@ -41,10 +45,13 @@ else:
                     folder_id = "1o0cuz9ltekMieUAPKV556-ncwqW9RIsx"
                     file_name_alunos = "alunos.xlsx"
                     file_name_atividade = "atividade.xlsx"
+                    file_name_email = "email.xlsx"
                     file_data_alunos = download_file_by_name(service, folder_id, file_name_alunos)
                     file_data_atividade = download_file_by_name(service, folder_id, file_name_atividade)
+                    file_data_email = download_file_by_name(service, folder_id, file_name_email)
                     df_alunos = pd.read_excel(file_data_alunos) if file_data_alunos else None
                     df_atividade = pd.read_excel(file_data_atividade) if file_data_atividade else None
+                    df_email = pd.read_excel(file_data_email) if file_data_email else None
 
                     if df_alunos is not None:
                         pass
@@ -54,9 +61,13 @@ else:
                         pass
                     else:
                         st.error(f"Erro ao carregar o arquivo '{file_name_atividade}'.")
+                    if df_email is not None:
+                        pass
+                    else:
+                        st.error(f"Erro ao carregar o arquivo '{file_name_email}'.")
 
-                    return df_atividade, df_alunos
-            df_atividade, df_alunos = data_load()
+                    return df_atividade, df_alunos, df_email
+            df_atividade, df_alunos, df_email = data_load()
 
             with st.container(): # CONFIGURAÇÕES de DATA
 
@@ -216,6 +227,18 @@ else:
             else:
                 st.success("Dados carregados com sucesso!")
 
+        with st.container(): # Funções para verificar envio
+        
+            def list_files_in_folder(service, folder_id):
+                query = f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.folder'"
+                response = service.files().list(q=query, fields="files(id, name)").execute()
+                return response.get('files', [])
+
+            def list_parquet_files(service, folder_id):
+                query = f"'{folder_id}' in parents and mimeType='application/octet-stream' and name contains '.parquet'"
+                response = service.files().list(q=query, fields="files(id, name)").execute()
+                return response.get('files', [])
+     
         with st.container():  # FILTROS ALUNO, DATA
 
             agora = datetime.now()
@@ -231,28 +254,26 @@ else:
             }
 
             selecoes = {}  
-            for label, (col, options) in filtros.items():
+            with st.expander("**:blue[FILTROS]**"):  # FILTROS
 
-                default_value = 'TODOS'
-                if label == 'Ano':
-                    default_value = ano_atual if ano_atual in options else 'TODOS'
-                elif label == 'Mês':
-                    default_value = mes_atual if mes_atual in options else 'TODOS'
+                for label, (col, options) in filtros.items():
+                    default_value = 'TODOS'
+                    if label == 'Ano':
+                        default_value = ano_atual if ano_atual in options else 'TODOS'
+                    elif label == 'Mês':
+                        default_value = mes_atual if mes_atual in options else 'TODOS'
 
-                selecionado = st.selectbox(
-                    f'Selecione o {label}:', 
-                    options, 
-                    index=options.index(default_value) if default_value in options else 0,  
-                    key=f"filtro_{label}" 
-                )
-                selecoes[label] = selecionado 
-                if selecionado != 'TODOS':
-                    df = df[df[col] == selecionado]
+                    selecionado = st.selectbox(
+                        f'Selecione o {label}:', 
+                        options, 
+                        index=options.index(default_value) if default_value in options else 0,  
+                        key=f"filtro_{label}" 
+                    )
+                    selecoes[label] = selecionado 
+                    if selecionado != 'TODOS':
+                        df = df[df[col] == selecionado]
 
         df = df.sort_values(by="HORAS", ascending=False)
-
-
-
 
         with st.container(): # CONSOLIDADO
             df_atividade = df.groupby('ATIVIDADE').agg({
@@ -284,7 +305,7 @@ else:
                     dias = [dia for dia in periodo_execucao.split(', ') if dia != 'Durante o mês']
                     if dias:
                         st.warning(f"A atividade '{row['Nome da Atividade']}' teve 'Durante o mês' removido, pois outros dias foram especificados por outros membros.")
-                        return ', '.join(dias)
+                        return ', '.join(dias)   
                 return periodo_execucao
 
             df_atividade['Período de Execução'] = df_atividade.apply(clean_periodo_execucao, axis=1)
@@ -310,5 +331,162 @@ else:
                 mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
             )
 
+        with st.container(): # Validação de relatorios enviados
+
+            selecione_ano = selecoes['Ano'] 
+            selecione_mes = selecoes['Mês']  
+            alunos = df_alunos['NOME'].tolist()
+            total_alunos = len(alunos)
+            pastas_alunos = list_files_in_folder(service, FOLDER_ID)
+            nomes_pastas = {pasta['name']: pasta['id'] for pasta in pastas_alunos}
+            alunos_enviaram = []
+            alunos_faltantes = []
+
+            for aluno in alunos:
+                if aluno in nomes_pastas:
+                    pasta_aluno_id = nomes_pastas[aluno]
+                    arquivos = list_parquet_files(service, pasta_aluno_id)
+
+                    encontrou_relatorio = False
+                    for arquivo in arquivos:
+                        nome_arquivo = arquivo['name']
+                        partes = nome_arquivo.split("_")
+
+                        if len(partes) >= 3:  
+                            numero_mes = partes[1]  
+                            ano_no_arquivo = partes[2].split(".")[0]  
+
+                            if str(numero_mes) == str(selecione_mes) and str(ano_no_arquivo) == str(selecione_ano):
+                                encontrou_relatorio = True
+                                break  
+
+                    if encontrou_relatorio:
+                        alunos_enviaram.append(aluno)
+                    else:
+                        alunos_faltantes.append(aluno)
+                else:
+                    alunos_faltantes.append(aluno)
+
+            enviados = len(alunos_enviaram)
+            st.warning(f"📩 Relatórios enviados para {selecione_mes}/{selecione_ano}: **{enviados}/{total_alunos}**")
+
+            if alunos_faltantes:
+                st.error(f"🚨 Alunos que ainda não enviaram o relatório de {selecione_mes}/{selecione_ano}:")
+                for aluno in alunos_faltantes:
+                    st.write(f"- {aluno}")
+                    
+
+            if not alunos_faltantes:
+                st.success("🎉 Todos os alunos enviaram o relatório!")
+
+                ### 🔹 Função para buscar o JSON de envios no Google Drive
+                def get_envios_from_drive():
+                    query = f"'{FOLDER_ID}' in parents and name='envios.json' and mimeType='application/json'"
+                    results = service.files().list(q=query, fields="files(id, name)").execute()
+                    files = results.get("files", [])
+
+                    if files:
+                        file_id = files[0]["id"]
+                        request = service.files().get_media(fileId=file_id)
+                        file_data = request.execute()
+                        file_content = json.loads(file_data.decode("utf-8"))
+                        return file_content, file_id
+                    return {}, None  # Retorna um dicionário vazio se não existir
+
+                ### 🔹 Função para salvar o JSON no Google Drive
+                def save_envios_to_drive(data, file_id=None):
+                    json_data = json.dumps(data, indent=4)
+                    file_stream = io.BytesIO(json_data.encode("utf-8"))
+
+                    media = MediaIoBaseUpload(file_stream, mimetype="application/json")
+
+                    if file_id:
+                        service.files().update(fileId=file_id, media_body=media).execute()
+                    else:
+                        file_metadata = {
+                            "name": "envios.json",
+                            "mimeType": "application/json",
+                            "parents": [FOLDER_ID],
+                        }
+                        service.files().create(body=file_metadata, media_body=media).execute()
+
+                envios, file_id = get_envios_from_drive()
+
+                nome_relatorio = f"relatorio_consolidado_{mes_escolhido}-{ano_escolhido}.xlsx"
+
+                def enviar_email(df_atividade, mes_escolhido, ano_escolhido):
+
+                    SMTP_SERVER = st.secrets["EMAIL"]["SMTP_SERVER"]
+                    SMTP_PORT = st.secrets["EMAIL"]["SMTP_PORT"]
+                    EMAIL_SENDER = st.secrets["EMAIL"]["EMAIL_SENDER"]
+                    EMAIL_PASSWORD = st.secrets["EMAIL"]["EMAIL_PASSWORD"]
+                    EMAIL_DESTINATARIO = ", ".join(df_email["EMAIL"].dropna().tolist())
+
+                    def to_excel(df):
+                        output = io.BytesIO()
+                        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+                        df.to_excel(writer, sheet_name='Sheet1', index=False)
+                        writer.close()
+                        return output.getvalue()
+
+                    df_excel = to_excel(df_atividade)
+
+                    msg = MIMEMultipart()
+                    msg["From"] = EMAIL_SENDER
+                    msg["To"] = EMAIL_DESTINATARIO
+                    msg["Subject"] = f"Relatório Consolidado {mes_escolhido}/{ano_escolhido}"
+
+                    mensagem = f"""
+                        Este é um e-mail gerado automaticamente. Por favor, não responda a esta mensagem.
+
+                        Prezado(a),
+
+                        Segue em anexo o relatório consolidado referente a {mes_escolhido}/{ano_escolhido}. Caso haja qualquer inconsistência ou dúvida, entre em contato com a equipe responsável.
+
+                        Agradecemos sua atenção.
+
+                        Atenciosamente,  
+                        Programa de Educação Tutorial  
+                        PET - ECONOMIA
+                        """
+                    msg.attach(MIMEText(mensagem, "plain"))
+
+                    part = MIMEBase("application", "octet-stream")
+                    part.set_payload(df_excel)
+                    encoders.encode_base64(part)
+                    part.add_header(
+                        "Content-Disposition",
+                        f"attachment; filename=relatorio_consolidado_{mes_escolhido}-{ano_escolhido}.xlsx"
+                    )
+                    msg.attach(part)
+
+                    try:
+                        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+                        server.starttls()
+                        server.login(EMAIL_SENDER, EMAIL_PASSWORD)
+                        server.sendmail(EMAIL_SENDER, EMAIL_DESTINATARIO, msg.as_string())
+                        server.quit()
+                        st.success("✅ E-mail enviado com sucesso!")
+                        st.balloons()
+                    except Exception as e:
+                        st.error(f"❌ Erro ao enviar o e-mail, entre em contato com o Desenvolvedor. {e}")
+
+                if nome_relatorio in envios:
+                    data_envio = envios[nome_relatorio]
+                    st.warning(f"⚠️ O relatório `{nome_relatorio}` já foi enviado em `{data_envio}`. Nenhum e-mail foi enviado novamente.")
+                    
+                    if st.button("🔄 Reenviar Relatório"):
+                        enviar_email(df_atividade, mes_escolhido, ano_escolhido)
+                        data_envio = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                        envios[nome_relatorio] = data_envio
+                        save_envios_to_drive(envios, file_id)
+                        st.success(f"✅ Relatório `{nome_relatorio}` reenviado em `{data_envio}`.")
+
+                else:
+                    enviar_email(df_atividade, mes_escolhido, ano_escolhido)
+                    data_envio = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    envios[nome_relatorio] = data_envio
+                    save_envios_to_drive(envios, file_id)
+                    st.success(f"✅ Relatório `{nome_relatorio}` enviado com sucesso em `{data_envio}`.")
 
     display_sidebar()
